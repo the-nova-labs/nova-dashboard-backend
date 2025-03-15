67fe08f889b89bc008652c767ac012b0f8d1fc46
pycorn0729 <hayesdominique0729@gmail.com>
2025-03-15 11:21:39 -0500

    feat: add bittensor stuff

--- app/core/constants.py
+++ app/core/constants.py
@@ -1,2 +1,6 @@
 #DATABASE_URL = "sqlite:///test-nova-leaderboard.db"
 DATABASE_URL = "postgresql://postgres:nova@localhost:5432/postgres"
+NETWORK = "local"
+SUBNET_UID = 68
+NEURON_EPOCH_LENGTH = 25
+BLOCK_IN_SECONDS = 12
