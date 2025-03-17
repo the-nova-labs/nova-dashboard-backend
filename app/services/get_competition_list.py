0a2c8d0e3217f27d4c0b5ae40a7db7003366ca1e
pycorn0729 <hayesdominique0729@gmail.com>
2025-03-17 08:50:59 -0500

    feat: update competition list

--- app/services/get_competition_list.py
+++ app/services/get_competition_list.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session, aliased
 from app.models.models import Competition, Protein, CompetitionMetadata        
-
+from app.core.metagraph import METAGRAPH
 
 def get_competition_list(db: Session):
     """Fetch all competitions along with their target and anti-target proteins."""
@@ -13,11 +13,12 @@ def get_competition_list(db: Session):
             Competition.id,
             Competition.epoch_number,
             target_protein_alias.protein.label("target_protein"),
-            anti_target_protein_alias.protein.label("anti_target_protein")
+            anti_target_protein_alias.protein.label("anti_target_protein"),
+            CompetitionMetadata.best_hotkey,
         )
-        
         .join(target_protein_alias, target_protein_alias.id == Competition.target_protein_id)  # Join target protein
         .join(anti_target_protein_alias, anti_target_protein_alias.id == Competition.anti_target_protein_id)  # Join anti-target protein
+        .outerjoin(CompetitionMetadata, CompetitionMetadata.competition_id == Competition.id)
         .order_by(Competition.epoch_number.desc())  # Sort by latest epoch first
         .all()
     )
@@ -28,6 +29,8 @@ def get_competition_list(db: Session):
             "epoch_number": competition.epoch_number, 
             "target_protein": competition.target_protein,
             "anti_target_protein": competition.anti_target_protein,
+            "best_hotkey": competition.best_hotkey,
+            "best_uid": METAGRAPH.get_uid(competition.best_hotkey) if competition.best_hotkey else None
         } 
         for competition in competitions
     ]
